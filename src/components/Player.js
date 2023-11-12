607c5f5b72faa535ee6a2da88deb173082747b1c
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-11-11 22:22:02 -0600

    first commit

---
+++ src/components/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div>
+        <iframe style={{border: "0", width: "350px", height: "470px"}} src="https://bandcamp.com/EmbeddedPlayer/album=2005259206/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/" seamless><a href="https://claycamero.bandcamp.com/album/fortitude-2">Fortitude by Clay Camero</a></iframe>
+    </div>
+  )
+}
+
+export default Player
